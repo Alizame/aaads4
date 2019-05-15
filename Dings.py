1da4cc99d6256a516522d9434029aa21ed0da54e
Maximilian Frömelt <maxifroemelt@gmail.com>
2019-05-15 17:38:03 +0200

    Added modify functionality to TUI

--- Dings.py
+++ Dings.py
@@ -1,13 +1,6 @@
 from LinkedList import LinkedList
 from StudiObject import StudiObject
 
-
-def empty():
-    raise NotImplementedError("Ey du Wurst! Hier is nüx.")
-
-change_name = empty
-change_matr = empty
-
 _sort_name = lambda studi: studi.get_name()
 _sort_matr = lambda studi: studi.get_matr()
 no = lambda x: x
@@ -29,12 +22,35 @@ class TUI:
         self.ll_by_name.attach_sorted(studi, key=_sort_name)
         self.ll_by_matr.attach_sorted(studi, key=_sort_matr)
 
-    def _del(self, lst, key, cast_to=no):  # deletes all occurences!!!!!!!!!!?
+    def _del_search(self, key, cast_to=no):  # deletes all occurences!!!!!!!!!!?
         what = cast_to(input(">>> "))
 
         self.unsorted_array = [studi for studi in self.unsorted_array if not what == key(studi)]
         self.ll_by_matr.delete(what, key=key)
         self.ll_by_name.delete(what, key=key)
+
+    def __del_node(self, node):
+        self.unsorted_array.remove(node.get_data())
+        self.ll_by_name.delete_node(node)
+        self.ll_by_matr.delete_node(node)
+
+    def _modify_name(self):
+        print("Wähle Nummer:")
+        self._print(self.ll_by_name)
+        studi_node = list(self.ll_by_name)[int(input(">>> "))-1]
+        new_name = input("Ändere {}: neuer Name: \n>>> ".format(studi_node))
+        self.__del_node(studi_node)  # delete from lists
+        studi_node.get_data().set_name(new_name)  # change student
+        self._add(studi_node.get_data())  # reinsert student (to maintain sorted lists)
+
+    def _modify_matr(self):
+        print("Wähle Nummer:")
+        self._print(self.ll_by_matr)
+        studi_node = list(self.ll_by_matr)[int(input(">>> "))-1]
+        new_age = int(input("Ändere {}: neue matr: \n>>> ".format(studi_node)))
+        self.__del_node(studi_node)  # delete from lists
+        studi_node.get_data().set_matr(new_age)  # change student
+        self._add(studi_node.get_data())  # reinsert student (to maintain sorted lists)
 
     def __init__(self, filepath=None):
         if filepath is None:
@@ -57,13 +73,13 @@ class TUI:
             "N":  ("Neuen Studi einfügen",
                    self._add_new_studi),
             "LN": ("Studi nach Namen löschen",
-                   lambda: self._del(self.ll_by_name, _sort_name)),
+                   lambda: self._del_search(_sort_name)),
             "LM": ("Studi nache Matrikelnummer löschen",
-                   lambda: self._del(self.ll_by_matr, _sort_matr, int)),
+                   lambda: self._del_search(_sort_matr, int)),
             "MN": ("Studinamen ändern",
-                   change_name),
+                   self._modify_name),
             "MM": ("Matrikelnummer ändern",
-                   change_matr),
+                   self._modify_matr),
             "S":  ("Daten speichern", self._save),
             "E":  ("Programmende", exit)
         }
@@ -79,7 +95,7 @@ class TUI:
             print(" # {} - {}".format(key, menu_item[0]))
         inp = input("Please choose one of the options above: \n>>> ").upper()
         sel = self.options.get(inp, ("invalid", self._invalid_input))
-        print("Selected '{}':".format(sel[0]))
+        print("'{}' ausgewählt:".format(sel[0]))
         sel[1]()
 
     def _invalid_input(self):
